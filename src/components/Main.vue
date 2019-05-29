381ee56d381ff80b8a0fdb5b29334a350c7d9863
zirra <msullivan@heysulli.com>
2019-05-28 21:53:58 -0500

    updating includes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -21,7 +21,7 @@
 
 <script>
 export default {
-  name: 'hello',
+  name: 'main-view',
   data () {
     return {
       msg: 'Welcome to Your Vue.js PWA'
--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vuex from 'vuex'
+import App from '@/App'
+import router from '@/router'
+import store from '@/store'
 
+import VueQriously from 'vue-qriously'
+
+// import VueSocketio from 'vue-socket.io-extended'
+// import io from 'socket.io-client'
+
+// Vue.use(VueSocketio, io(process.env.SOCKET_SERVER))
+Vue.use(Vuex)
+Vue.use(VueQriously)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
